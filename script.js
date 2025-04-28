10f2c5f1de0d6ec43d1c8b5d0c8ba2188e55363d
MhlengweLibertyLees <libertyengetelo@gmail.com>
2025-04-28 20:55:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Dynamic welcome message based on the time of day
+window.onload = function() {
+    const welcomeMessage = document.getElementById('welcomeMessage');
+    const currentHour = new Date().getHours();
+    let message;
+
+    if (currentHour < 12) {
+        message = "Good Morning! Welcome to My Portfolio";
+    } else if (currentHour < 18) {
+        message = "Good Afternoon! Welcome to My Portfolio";
+    } else {
+        message = "Good Evening! Welcome to My Portfolio";
+    }
+
+    welcomeMessage.textContent = message;
+};
